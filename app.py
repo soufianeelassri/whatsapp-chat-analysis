3690b9807406855609f42edf7c72690452d6f5ad
Soufiane <soufianeelassrii@gmail.com>
2023-12-14 00:27:49 +0100

    Most Common Words | WordCloud

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import preprocessing
 import functions
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
 
 app = Flask(__name__)
 
@@ -20,45 +22,53 @@ def analyze():
         total_messages, total_words, media_shared, link_shared = functions.basic_stats(df)
 
         monthly_timeline = functions.monthly_timeline(df)
+        must_busy_month = functions.must_busy_month(df)
         daily_timeline = functions.daily_timeline(df)
-        most_busy_days = functions.most_busy_days(df)
+        most_busy_day = functions.most_busy_day(df)
+        most_common_words = functions.most_common_words(df)
 
         fig1, ax1 = plt.subplots(figsize=(10, 4))
         fig2, ax2 = plt.subplots(figsize=(10, 4))
         fig3, ax3 = plt.subplots(figsize=(10, 4))
         fig4, ax4 = plt.subplots(figsize=(10, 4))
+        fig5, ax5 = plt.subplots(figsize=(10, 4))
 
         ax1.plot(monthly_timeline['time'], monthly_timeline['message'], color='green')
         ax1.set_title('Monthly Timeline')
-        ax1.set_ylabel('Message')
 
         plot_filename1 = 'static/monthly_timeline_plot.png'
         ax1.figure.savefig(plot_filename1)
         plt.close(fig1)
 
-        ax2.plot(daily_timeline['only_date'], daily_timeline['message'], color='green')
-        ax2.set_title('Daily Timeline')
-        ax2.set_ylabel('Message')
+        ax2.bar(must_busy_month['month'], must_busy_month['message'], color='green')
+        ax2.set_title('Most Busy Month')
 
-        plot_filename2 = 'static/daily_timeline_plot.png'
+        plot_filename2 = 'static/most_busy_month_plot.png'
         ax2.figure.savefig(plot_filename2)
         plt.close(fig2)
 
-        ax3.bar(most_busy_days['day_name'], most_busy_days['message'], color='green')
-        ax3.set_title('Most Busy Days')
-        ax3.set_ylabel('Message')
+        ax3.plot(daily_timeline['only_date'], daily_timeline['message'], color='green')
+        ax3.set_title('Daily Timeline')
 
-        plot_filename3 = 'static/most_busy_days_plot.png'
+        plot_filename3 = 'static/daily_timeline_plot.png'
         ax3.figure.savefig(plot_filename3)
         plt.close(fig3)
 
-        ax4.bar(monthly_timeline['time'], monthly_timeline['message'], color='green')
-        ax4.set_title('Most Busy Months')
-        ax4.set_ylabel('Message')
+        ax4.bar(most_busy_day['day_name'], most_busy_day['message'], color='green')
+        ax4.set_title('Most Busy Day')
 
-        plot_filename4 = 'static/most_busy_months_plot.png'
+        plot_filename4 = 'static/most_busy_day_plot.png'
         ax4.figure.savefig(plot_filename4)
         plt.close(fig4)
+
+        wordcloud = WordCloud(width=800, height=400, background_color='lightyellow').generate_from_frequencies(dict(zip(most_common_words[0], most_common_words[1])))
+        ax5.imshow(wordcloud, interpolation='bilinear')
+        ax5.axis('off')
+        ax5.set_title('Most Common Words Plot')
+
+        plot_filename5 = 'static/most_common_words_plot.png'
+        ax5.figure.savefig(plot_filename5)
+        plt.close(fig5)
 
         return render_template(
                 'results.html',
@@ -70,7 +80,8 @@ def analyze():
                 plot_filename1 = plot_filename1,
                 plot_filename2 = plot_filename2,
                 plot_filename3 = plot_filename3,
-                plot_filename4 = plot_filename4
+                plot_filename4 = plot_filename4,
+                plot_filename5 = plot_filename5
             )
 
 if __name__ == '__main__':
--- functions.py
+++ functions.py
@@ -1,50 +1,62 @@
 from urlextract import URLExtract
 import pandas as pd
+from collections import Counter
+import emoji
 
 extract = URLExtract()
 
 def basic_stats(df):
 
-    # fetch the number of messages
     total_messages = df.shape[0]
-
-    # fetch the total number of words
     total_words = []
     for message in df['message']:
         total_words.extend(message.split())
-
-    # fetch number of media messages
     media_shared = df['message'].str.contains('<Media omitted>').sum()
-
-    # fetch number of links shared
     links_shared = df['message'].apply(lambda x: extract.find_urls(x)).explode().dropna().unique()
-
     return total_messages, len(total_words), media_shared, len(links_shared)
 
 def monthly_timeline(df):
 
     timeline = df.groupby(['year', 'month_num', 'month']).count()['message'].reset_index()
-
     time = []
     for i in range(timeline.shape[0]):
         time.append(timeline['month'][i] + "-" + str(timeline['year'][i]))
-
     timeline['time'] = time
-
     return timeline
 
 def daily_timeline(df):
 
-    timeline = df.groupby('only_date').count()['message'].reset_index()
+    daily_timeline = df.groupby('only_date').count()['message'].reset_index()
+    return daily_timeline
 
-    return timeline
-
-def most_busy_days(df):
+def most_busy_day(df):
 
     timeline = df.groupby('day_name').count()['message'].reset_index()
-
     custom_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     timeline['day_name'] = pd.Categorical(timeline['day_name'], categories=custom_order, ordered=True)
     timeline = timeline.sort_values(by='day_name')
-
     return timeline
+
+def must_busy_month(df):
+    timeline = df.groupby('month').count()['message'].reset_index()
+    timeline = timeline.sort_values(by='month', ascending=False)
+    return timeline
+
+def remove_emojis(text):
+    return emoji.demojize(text)
+
+def most_common_words(df):
+
+    f = open('stop_words.txt','r', encoding='utf-8')
+    stop_words = f.read()
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+    words = []
+    for message in temp['message']:
+        for word in message.lower().split():
+            cleaned_word = remove_emojis(word)
+            if not cleaned_word.startswith(':') and not cleaned_word.endswith(':'):
+                if cleaned_word not in stop_words:
+                    words.append(cleaned_word)
+    most_common_df = pd.DataFrame(Counter(words).most_common(20))
+    return most_common_df
